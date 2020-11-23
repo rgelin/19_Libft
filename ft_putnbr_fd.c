73552aaddb9143966540c4b962943176986e682f
rgelin <rgelin@student.s19.be>
2020-11-23 14:14:47 +0100

    part 2 2.0

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgelin <rgelin@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/22 15:05:29 by rgelin            #+#    #+#             */
+/*   Updated: 2020/11/23 13:03:15 by rgelin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long long nb;
+
+	nb  = n;
+	if (nb == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar_fd('-', fd);
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putchar_fd((n % 10) + '0', fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
+
+}
--- ft_split.c
+++ ft_split.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgelin <rgelin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/20 17:34:58 by rgelin            #+#    #+#             */
-/*   Updated: 2020/11/20 17:46:29 by rgelin           ###   ########.fr       */
+/*   Created: 2020/11/20 21:27:01 by rgelin            #+#    #+#             */
+/*   Updated: 2020/11/23 13:25:40 by rgelin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		nb_words(char *str, char c)
+static int      nb_words(char *str, char c)
 {
 	int i;
 	int nb_words;
@@ -26,13 +26,68 @@ static int		nb_words(char *str, char c)
 		if (str[i] == c)
 			nb_words++;
 		i++;
+
 	}
 	return (nb_words);
 }
 
-char	**ft_split(char const *s, char c)
+static int		*size_words(char *str, char c)
 {
-	
-	while (s[i] == c)
-		i++;
+	int i;
+	int j;
+	int count;
+	int *size_words;
+
+	j = 0;
+	i = 0;
+	if (!(size_words = malloc(sizeof(int) * (nb_words(str, c)))))
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] == c)
+		{
+			count = 0;
+			j++;
+			i++;
+		}
+		while(str[i] != c && str[i])
+		{
+			count++;
+			i++;
+		}
+		size_words[j] = count;
+	}
+	return (size_words);
+}
+
+char			**ft_split(char const *s, char c)
+{
+	int		i;
+	int		index;
+	int		j;
+	char	**res;
+	char	*str;
+	int		*size;
+
+	str = (char *)s;
+	size= size_words(str, c);
+	i = 0;
+	index = 0;
+	if (!(res = malloc(sizeof(char*) * (nb_words(str, c) + 1)))
+		|| !str[0])
+		return (0);
+	while (index < nb_words(str, c))
+	{
+		j = 0;
+		if (!(res[index] = malloc(sizeof(char) * size[index] + 1)))
+			return (NULL);
+		if (str[i] && str[i] == c)
+			i++;
+		while (str[i] && str[i] != c)
+			res[index][j++] = str[i++];
+		res[index][j] = '\0';
+		index++;
+	}
+	res[index] = 0;
+	return (res);
 }
