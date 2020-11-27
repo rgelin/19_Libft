0b92b68ace500775f3c01f1e967b1d0129faebe2
rgelin <rgelin@student.s19.be>
2020-11-27 14:40:32 +0100

    finish tout bon

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,36 +6,36 @@
 /*   By: rgelin <rgelin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 15:00:43 by rgelin            #+#    #+#             */
-/*   Updated: 2020/11/27 12:58:12 by rgelin           ###   ########.fr       */
+/*   Updated: 2020/11/27 14:36:27 by rgelin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *l, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_element;
 
-	if (lst == 0 || f == 0)
+	if (l == 0 || f == 0)
 		return (NULL);
-	if (!(new_element = ft_lstnew(f(lst->content))))
+	if (!(new_element = ft_lstnew(f(l->content))))
 	{
-		ft_lstclear(&lst, del);
+		ft_lstclear(&l, del);
 		return (NULL);
 	}
 	new_list = new_element;
-	lst = lst->next;
-	while (lst)
+	l = l->next;
+	while (l)
 	{
-		if (!(new_element = ft_lstnew(f(lst->content))))
+		if (!(new_element = ft_lstnew(f(l->content))))
 		{
-			ft_lstclear(&lst, del);
+			ft_lstclear(&l, del);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_element);
-		lst = lst->next;
+		l = l->next;
 	}
 	return (new_list);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,28 +6,28 @@
 /*   By: rgelin <rgelin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 12:47:45 by rgelin            #+#    #+#             */
-/*   Updated: 2020/11/25 23:31:38 by rgelin           ###   ########.fr       */
+/*   Updated: 2020/11/27 14:33:59 by rgelin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	size_t i;
 	size_t j;
 
 	i = 0;
-	if (((char *)needle)[0] == '\0')
-		return ((char *)haystack);
-	while (haystack[i] && i < len)
+	if (((char *)to_find)[0] == '\0')
+		return ((char *)str);
+	while (str[i] && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && i + j < len && needle[j]
-			&& haystack[i + j])
+		while (str[i + j] == to_find[j] && i + j < len && to_find[j]
+			&& str[i + j])
 			j++;
-		if (needle[j] == 0)
-			return (&((char *)haystack)[i]);
+		if (to_find[j] == 0)
+			return (&((char *)str)[i]);
 		i++;
 	}
 	return (0);
